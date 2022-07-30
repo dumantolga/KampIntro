f203d67ab134f540226079b0b2bf5551e8644fdd
Tolga Duman <tolga387@hotmail.com>
2022-07-30 21:34:31 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama
+            //değer tutucu, alias
+
+            string kategoriEtiketi = "hakan";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            double abc = 1.60;
+            bool sistemeGirisYapmısmi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmısmi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
