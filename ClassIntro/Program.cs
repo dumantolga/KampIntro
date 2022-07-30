57452c3f86456480cee0044abc36e12c439913cd
Tolga Duman <tolga387@hotmail.com>
2022-07-30 22:07:36 +0300

    classintro

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string adi = "tolga";
+int yasi = 25;
+
+Kurs kurs1 = new Kurs();
+kurs1.KursAdi = "C#";
+kurs1.Egitmen = "Engin Demiroğ";
+kurs1.IzlenmeOranı = 68;
+
+Kurs kurs2 = new Kurs();
+kurs2.KursAdi = "Java";
+kurs2.Egitmen = "Tolga";
+kurs2.IzlenmeOranı = 77;
+
+Kurs kurs3 = new Kurs();
+kurs3.KursAdi = "Python";
+kurs3.Egitmen = "Xmen";
+kurs3.IzlenmeOranı = 25;
+
+//Console.WriteLine(kurs1.KursAdi + " : " + kurs1.Egitmen);
+
+Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+foreach (var kurs in kurslar)
+{
+    Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen);
+}
+
+//Console.WriteLine("Hello, World!");
+class Kurs
+{
+    public string KursAdi { get; set; }
+    public string Egitmen { get; set; }
+    public int IzlenmeOranı { get; set; }
+
+}
