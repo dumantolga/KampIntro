18d0462b08c4183521033f4952f3efa4d38ee5b7
Tolga Duman <tolga387@hotmail.com>
2022-08-01 19:55:54 +0300

    Generic Count Simulation

---
+++ Generics/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Antakya");
+            sehirler.Add("Antakya");
+            sehirler.Add("Antakya");
+            sehirler.Add("Antakya");
+            sehirler.Add("Antakya");
+            Console.WriteLine(sehirler.Count);
+            
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Hatay");
+            sehirler2.Add("Hatay");
+            sehirler2.Add("Hatay");
+            sehirler2.Add("Hatay");
+            sehirler2.Add("Hatay");
+            sehirler2.Add("Hatay");
+            Console.WriteLine(sehirler2.Count);
+            
+        }
+
+
+    }
+    class MyList<T>//Generic class
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyList()
+        {
+           _array = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length+1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
+         
+        }
+
+
+    }
+}
