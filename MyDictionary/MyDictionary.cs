6c9eebe0f942b7c64f89e8a7bb4135e29e595922
Tolga Duman <tolga387@hotmail.com>
2022-08-04 19:19:33 +0300

    MyDictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class MyDictionary<K,V>
+    {
+        KeyValuePair<K,V> [] items;
+        
+        public MyDictionary()
+        {
+            items = new KeyValuePair<K, V>[0];
+        }
+
+        public void Add(K key,V value)
+        {
+            if (Control(key))
+            {
+                Configuration(key);
+                items[items.Length - 1] = new KeyValuePair<K,V>(key, value);
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz key değeri özel olmalı. Yani daha önceden eklenmiş bir key değerini tekrardan kullanamassınız.");
+            }
+        }
+
+        public void ShowList()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+
+        }
+
+        private void Configuration(K key)
+        {
+            KeyValuePair<K, V>[] tempArray = items;  // Geçici dizimizi oluşturduk
+            items = new KeyValuePair<K, V>[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];  //Geçici dizideki elemanları alıp asıl dizimize gri koyduk.
+            }
+
+        }
+        public bool Control(K key)
+        {
+            bool control = true;
+            for(int i = 0; i < items.Length; i++)
+            {
+                if (items[i].Key.ToString() == key.ToString())
+                {
+                    control = false;
+                }
+            }
+            return control;
+
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Adana");
+            myDictionary.Add(6, "Ankara");
+            myDictionary.Add(31, "Antakya");
+            myDictionary.Add(34, "İstanbul");
+
+
+            myDictionary.ShowList();
+            Console.WriteLine();
+
+
+        }
+    }
+}
