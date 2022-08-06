ee5f4a10a6878693c901b4601cb7e25408bd5aa2
Tolga Duman <tolga387@hotmail.com>
2022-08-06 16:34:20 +0300

    Dictionary (MyDictionary)

---
+++ DictionaryDeneme/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DictionaryDeneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> users = new MyDictionary<int, string>();
+
+            users.Add(12, "Merve");
+            users.Add(31, "Tolga");
+            users.Add(79, "Betül");
+
+            users.Print();
+        }
+    }
+}
