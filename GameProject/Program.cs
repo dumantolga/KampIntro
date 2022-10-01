a6c59d3f650563d6a062d81154b2ae251c3eea46
Tolga Duman <tolga387@hotmail.com>
2022-10-02 00:58:13 +0300

    İnterface OOP Simülasyon

---
+++ GameProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GameProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateValidationManager());
+            gamerManager.Add(new Gamer { 
+                Id = 1,
+                BirthYear = 1998,
+                FirstName ="TOLGA",
+                LastName ="DUMAN",
+                IdentityNumber =12345
+            }); 
+        }
+    }
+}
+//1.Oyuncuların sisteme kayıt olabileceği, bilgilerini güncelleyebileceği,
+//kayıtlarını silebileceği bir ortamı simule ediniz. Müşteri bilgilerinin doğruluğunu
+//e-devlet sistemlerini kullanarak doğrulama yapmak istiyoruz.
+//(E-devlet sistemlerinde doğrulama TcNo, Ad, Soyad, DoğumYılı bilgileriyle yapılır.
+//Bunu yapacak servisi simule etmeniz yeterlidir.)
+
+//2.Oyun satışı yapılabilecek satış ortamını simule ediniz.
+//( Yapılan satışlar oyuncu ile ilişkilendirilmelidir.
+//Oyuncunun parametre olarak metotta olmasını kastediyorum.)
+
+//3.Sisteme yeni kampanya girişi, kampanyanın silinmesi ve güncellenmesi imkanlarını simule ediniz.
+
+//4.Satışlarda kampanya entegrasyonunu simule ediniz.
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1997 && gamer.FirstName =="TOLGA" 
+                && gamer.LastName=="DUMAN" && gamer.IdentityNumber==12345)
+            {
+                return true;
+            }
+            else
+            {
+                  return false;
+            }
+        }
+    }
+}
